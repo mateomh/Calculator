81f6df3de521637d7a27adb019fa555e8a3c8a6a
mateomh <mmojicah.84@gmail.com>
2020-11-12 16:37:04 -0500

    Add content to the app component

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Display from './display';
+import Panel from './panel';
+
+export default function App() {
+  return (
+    <fragment>
+      <Display />
+      <Panel />
+    </fragment>
+  );
+}
