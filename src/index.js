59fc75ea7c735797f1fa56093a0acaa7e0422287
mateomh <mmojicah.84@gmail.com>
2020-11-12 13:22:44 -0500

    Add some content to index.js to check the application is running

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+function Test() {
+  return <div>Hello World From React</div>;
+}
+
+ReactDOM.render(
+  <Test />,
+  document.getElementById('root'),
+);
