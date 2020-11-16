d0485cd3cdefe52efd6bcba1ce26bb56909b2a4a
mateomh <mmojicah.84@gmail.com>
2020-11-16 11:22:00 -0500

    Add event handler as a prop to the panel component

--- src/components/app.js
+++ src/components/app.js
@@ -2,11 +2,27 @@ import React from 'react';
 import Display from './display';
 import Panel from './panel';
 
-export default function App() {
-  return (
-    <fragment>
-      <Display />
-      <Panel />
-    </fragment>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    return event.target.value;
+  }
+
+  render() {
+    return (
+      <fragment>
+        <Display />
+        <Panel clickHandler={this.handleClick} />
+      </fragment>
+    );
+  }
 }
--- src/components/panel.js
+++ src/components/panel.js
@@ -1,19 +1,21 @@
+import PropTypes from 'prop-types';
 import ButtonKey from './button';
 
-export default function Panel() {
+export default function Panel(props) {
+  const { clickHandler } = props;
   return (
     <fragment>
       <div className="button-row">
-        <ButtonKey value="AC" type="button type3" />
-        <ButtonKey value="+/-" type="button type3" />
-        <ButtonKey value="%" type="button type3" />
-        <ButtonKey value="/" type="button type2" />
+        <ButtonKey value="AC" type="button type3" clickCallBack={clickHandler} />
+        <ButtonKey value="+/-" type="button type3" clickCallBack={clickHandler} />
+        <ButtonKey value="%" type="button type3" clickCallBack={clickHandler} />
+        <ButtonKey value="/" type="button type2" clickCallBack={clickHandler} />
       </div>
       <div className="button-row">
-        <ButtonKey value="7" type="button type1" />
-        <ButtonKey value="8" type="button type1" />
-        <ButtonKey value="9" type="button type1" />
-        <ButtonKey value="X" type="button type2" />
+        <ButtonKey value="7" type="button type1" clickCallBack={clickHandler} />
+        <ButtonKey value="8" type="button type1" clickCallBack={clickHandler} />
+        <ButtonKey value="9" type="button type1" clickCallBack={clickHandler} />
+        <ButtonKey value="X" type="button type2" clickCallBack={clickHandler} />
       </div>
       <div className="button-row">
         <ButtonKey value="4" type="button type1" />
@@ -22,16 +24,20 @@ export default function Panel() {
         <ButtonKey value="-" type="button type2" />
       </div>
       <div className="button-row">
-        <ButtonKey value="1" type="button type1" />
-        <ButtonKey value="2" type="button type1" />
-        <ButtonKey value="3" type="button type1" />
-        <ButtonKey value="+" type="button type2" />
+        <ButtonKey value="1" type="button type1" clickCallBack={clickHandler} />
+        <ButtonKey value="2" type="button type1" clickCallBack={clickHandler} />
+        <ButtonKey value="3" type="button type1" clickCallBack={clickHandler} />
+        <ButtonKey value="+" type="button type2" clickCallBack={clickHandler} />
       </div>
       <div className="button-row last-row">
-        <ButtonKey value="0" type="button type1" />
-        <ButtonKey value="." type="button type1" />
-        <ButtonKey value="=" type="button type2" />
+        <ButtonKey value="0" type="button type1" clickCallBack={clickHandler} />
+        <ButtonKey value="." type="button type1" clickCallBack={clickHandler} />
+        <ButtonKey value="=" type="button type2" clickCallBack={clickHandler} />
       </div>
     </fragment>
   );
 }
+
+Panel.propTypes = {
+  clickHandler: PropTypes.func.isRequired,
+};
