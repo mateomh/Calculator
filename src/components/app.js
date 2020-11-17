a0aeb694ad598f00c3d8dfc385e5ae0aadf9e50e
mateomh <mmojicah.84@gmail.com>
2020-11-17 11:56:17 -0500

    Refactor the App component to use the useState hook

--- src/components/app.js
+++ src/components/app.js
@@ -1,34 +1,62 @@
 /* eslint-disable no-nested-ternary */
-import React from 'react';
+// import React from 'react';
+// import Display from './display';
+// import Panel from './panel';
+// import calculate from '../logic/calculate';
+
+// export default class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       total: '',
+//       next: '',
+//       operation: '',
+//     };
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick(event) {
+//     const { value: buttonValue } = event.target;
+//     const newState = calculate(this.state, buttonValue);
+//     this.setState(newState);
+//     console.log(newState);
+//   }
+
+//   render() {
+//     const { total, next } = this.state;
+//     return (
+//       <>
+//         <Display result={next === '' ? total === '' ? undefined : total : next} />
+//         <Panel clickHandler={this.handleClick} />
+//       </>
+//     );
+//   }
+// }
+import { useState } from 'react';
+import calculate from '../logic/calculate';
 import Display from './display';
 import Panel from './panel';
-import calculate from '../logic/calculate';
 
-export default class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      total: '',
-      next: '',
-      operation: '',
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+const App = () => {
+  const [state, setState] = useState({
+    total: '',
+    next: '',
+    operation: '',
+  });
 
-  handleClick(event) {
-    const { value: buttonValue } = event.target;
-    const newState = calculate(this.state, buttonValue);
-    this.setState(newState);
-    console.log(newState);
-  }
+  const { total, next } = state;
 
-  render() {
-    const { total, next } = this.state;
-    return (
-      <>
-        <Display result={next === '' ? total === '' ? undefined : total : next} />
-        <Panel clickHandler={this.handleClick} />
-      </>
-    );
-  }
-}
+  const handleClick = ({ target: { value: buttonValue } }) => {
+    const newState = calculate(state, buttonValue);
+    setState(newState);
+  };
+
+  return (
+    <>
+      <Display result={next === '' ? total === '' ? undefined : total : next} />
+      <Panel clickHandler={handleClick} />
+    </>
+  );
+};
+
+export default App;
